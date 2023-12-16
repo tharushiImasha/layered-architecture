bbdbe137eff85415cde3ec9473deb030727a33d0
Tharushi Imasha <tharushi.byb@gmail.com>
2023-12-16 16:53:57 +0530

    first commit

---
+++ src/main/java/com/example/layeredarchitecture/dao/ItemDaoImpl.java
@@ -0,0 +1,21 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ItemDaoImpl {
+    public String genarateId() throws SQLException, ClassNotFoundException, SQLException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        ResultSet rst = connection.createStatement().executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("code");
+            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newItemId);
+        } else {
+            return "I00-001";
+        }
+    }
+}
